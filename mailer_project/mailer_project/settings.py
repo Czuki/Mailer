e7bc903b1b5e5d7ceb757d90bb27faa9e3c4d822
Czuki <bart3322@gmail.com>
2021-03-05 19:55:38 +0100

    Update settings.py

--- mailer_project/mailer_project/settings.py
+++ mailer_project/mailer_project/settings.py
@@ -106,8 +106,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'mailerdjang@gmail.com'
-EMAIL_HOST_PASSWORD = 'dQyfen5z8kURxEF'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 
 # Internationalization
